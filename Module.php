445ce8938db3199c66a6180a91d346d033e8e938
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:46:01 +0200

    Added CS Fixer check to pre-commit

--- Module.php
+++ Module.php
@@ -16,18 +16,18 @@ namespace Aurora\Modules\Tasks;
  */
 class Module extends \Aurora\System\Module\AbstractModule
 {
-	public function init() {
-		
-	}
+    public function init()
+    {
+    }
 
-	/**
-	 * Obtains list of module settings for authenticated user.
-	 * 
-	 * @return array
-	 */
-	public function GetSettings()
-	{
-		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
-		return null;
-	}	
+    /**
+     * Obtains list of module settings for authenticated user.
+     *
+     * @return array
+     */
+    public function GetSettings()
+    {
+        \Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
+        return null;
+    }
 }
